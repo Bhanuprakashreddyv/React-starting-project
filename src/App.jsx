9ebd8ff8f99a51cfc8ffff1abe6756513d427c5a
Bhanuprakashreddyv <v.bhanureddy184@gmail.com>
2023-10-30 08:25:29 +0000

    Update Expamles Container with Data.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import reactImage from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
+import { EXAMPLES } from './data';
+import {useState} from 'react';
 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  };
+
   return (
     <div>
       <Header />
@@ -21,6 +29,22 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          <div id='tab-content'>
+           <h3>{EXAMPLES[selectedTopic].title}</h3>
+           <p>{EXAMPLES[selectedTopic].description}</p>
+           <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+           </pre>
+          </div>
         </section>
       </main>
     </div>
